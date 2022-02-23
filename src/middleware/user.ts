2ea4e513873c3a73521691322b358bc0fa2a252f
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-02-23 15:22:12 +0100

    bug fix

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { decodeToken } from './../services/jwt';
 
-export const checkIfUser = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const checkIfUser = async (req: any, res: express.Response, next: express.NextFunction) => {
     try {
         const token = req.headers.authorization;
         if (!token) return res.status(400).json({ message: "Token Missing. Please Sign In Again To Access This Page." });
