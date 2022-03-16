7f2e7b1a5660279311357b632a6909b932404b29
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-03-16 13:34:03 +0100

    bug fix

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -2,22 +2,51 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 import { validationResult } from 'express-validator';
 import express from 'express';
+import { s3Instance } from './../utilities/s3';
+import fs from 'fs';
+import path from 'path';
 
 export const createPost = async (req: any, res: express.Response, next: express.NextFunction) => {
     try {
-        const newPost = await prisma.post.create({
-            data: {
-                title: req.body.title,
-                content: req.file.location,
-                user: {
-                    connect: {
-                        email: req.body.user.email
-                    }
-                }
-            }
+        const fileExtension = req.file.originalname.split('.')[1];
+        const filePath = `${req.file.destination}${req.file.filename}`;
+        const absoluteFilePath = path.normalize(filePath);
+
+        const fileStream = fs.createReadStream(absoluteFilePath);
+        fileStream.on('error', function (error) {
+            console.log("File Error", error);
         });
 
-        return res.status(200).json({ message: 'Post Created Successfully!', newPost });
+        let contentType = 'application/octet-stream';
+        if (fileExtension == 'png' || fileExtension == 'jpg' || fileExtension == 'gif') contentType = "image/" + fileExtension;
+        if (fileExtension == 'mpeg') contentType = "video/" + fileExtension;
+
+        const uploadParams = {
+            Bucket: String(process.env.S3_BUCKET),
+            Key: `${Date.now().toString()}.${fileExtension}`,
+            Body: fileStream,
+            ContentType: contentType,
+        }
+
+        s3Instance.upload(uploadParams, async function (err: any, data: any) {
+            fs.unlink(absoluteFilePath, (err) => {
+                if (err) console.log(err);
+            });
+
+            const newPost = await prisma.post.create({
+                data: {
+                    title: req.body.title,
+                    content: data.Location,
+                    user: {
+                        connect: {
+                            email: req.body.user.email
+                        }
+                    }
+                }
+            });
+
+            if (newPost) return res.status(200).json({ message: 'Post Created Successfully!', newPost });
+        });
     } catch (error) {
         next(error);
     }
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -2,10 +2,24 @@ import express from 'express';
 import { body } from 'express-validator';
 import { checkIfUser } from './../middleware/user';
 import { createPost, getPost } from './../controllers/post';
-import { uploadFile } from './../utilities/multer';
 const router = express.Router();
 
-router.post('/api/v1/posts/upload', uploadFile.single('file'), checkIfUser, createPost);
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/data/uploads/');
+    },
+    filename: function (req, file, cb) {
+        const fileExtension = file.originalname.split('.')[1];
+        const filePath = `${Date.now().toString()}.${fileExtension}`;
+        cb(null, filePath);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/api/v1/posts/upload', upload.single('file'), checkIfUser, createPost);
 router.post('/api/v1/posts/download', body('fileId').isAscii(), checkIfUser, getPost);
 
 export { router };
